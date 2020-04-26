c51366de863651d97735f8440eb437fd10a76f14
sakko <saklism@gmail.com>
2020-04-26 14:34:44 +0700

    finished ep 17

---
+++ pages/inspire.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div>
+    <client-only>
+      <my-prism-editor :code="code" language="js"></my-prism-editor>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      code: '// inspire\nconst x = 10; x++; x--;'
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped></style>
---
+++ plugins/import-vue-prism-editor.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import 'prismjs'
+import 'prismjs/themes/prism.css'
+import PrismEditor from 'vue-prism-editor'
+import 'vue-prism-editor/dist/VuePrismEditor.css' // import the styles
+Vue.component('my-prism-editor', PrismEditor)
